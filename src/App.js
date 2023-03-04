981e45893af48f8cb4078ff4016d4194981051b2
Akani231 <julieberry231@gmail.com>
2023-03-04 23:52:56 +0200

    Imported react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import "./App.css";
 import Footer from "./Footer";
 import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container-wrapper">
+    <div class="App">
+      <div class="container-wrapper">
         <h1>🌏 Weather Forecast</h1>
-        <div className="content">
+        <div class="content">
           <Weather />
         </div>
       </div>
