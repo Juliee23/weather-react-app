8bc666b13db825fa14dcc22aaff641bb83b84e4d
Akani231 <julieberry231@gmail.com>
2023-03-07 20:59:44 +0200

    Changed Attributes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
+import WeatherTemp from "./WeatherTemp";
 
 export default function WeatherInfo(props) {
   return (
