de613baa70bcd3b111ca03f70587c0ddefa8d7ee
Akani231 <julieberry231@gmail.com>
2023-03-07 21:15:30 +0200

    Edited Default City

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Soweto" />
 
         <footer>
           This project was coded by Juliet Akani Ngomani and is{" "}
